5715107aea1d6b1ae1fc6204e0542309d1272300
Alex <sasha89253215539@yandex.ru>
2021-06-05 20:36:47 +0300

    desctop done + mobile first header done

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,11 @@
-const App = {
+const app = Vue.createApp ({
   data() {
     return {
       logo: "MOVEE",
       navLinks: [
         {
           text: "Цены",
-          link: "www.price.com",
+          link: "#prices",
         },
         {
           text: "Автопарк",
@@ -13,14 +13,15 @@ const App = {
         },
         {
           text: "Отзывы",
-          link: "#",
+          link: "#reviews",
         },
         {
           text: "Контакты",
-          link: "#",
+          link: "#contact",
         },
       ],
       number: "8(800)-080-12-13",
+      emailMain: 'hello@movee.ru',
       headertitle01: "Квартирные",
       headertitle02: "переезды",
       headersubtitle: "По Москве и области",
@@ -65,6 +66,100 @@ const App = {
           text: "Опытные грузчики быстро расставят мебель и уберут мусор от распаковки.",
         },
       ],
+      autoparkSubtitle: '10 видов автомобилей от 800 кг до 10 тонн',
+      autoparkCarouselNav: [
+        {
+          text: 'Газели',
+          id: 'autopark-1',
+        },
+        {
+          text: 'Каблуки',
+          id: 'autopark-2',
+        },
+        {
+          text: 'Грузовики',
+          id: 'autopark-3',
+        },
+      ],
+      autoparkCarouselSlide: [
+        {
+          title: 'Газель 3 метра',
+          list: [
+            {
+              listItem: 'Ширина 2 м',
+            },
+            {
+              listItem: 'Высота 2 м',
+            },
+            {
+              listItem: 'Объем 16 м³',
+            },
+            {
+              listItem: 'Грузоподъемность 1,5 т',
+            },
+          ],
+          btn: '',
+          autoImg: 'auto-01.png',
+        },
+        {
+          title: 'Каблук 2 метра',
+          list: [
+            {
+              listItem: 'Ширина 2 м',
+            },
+            {
+              listItem: 'Высота 2 м',
+            },
+            {
+              listItem: 'Объем 16 м³',
+            },
+            {
+              listItem: 'Грузоподъемность 1,5 т',
+            },
+          ],
+          btn: '',
+          autoImg: 'auto-03.png',
+        },        {
+          title: 'Грузовик 4 метра',
+          list: [
+            {
+              listItem: 'Ширина 2 м',
+            },
+            {
+              listItem: 'Высота 2 м',
+            },
+            {
+              listItem: 'Объем 16 м³',
+            },
+            {
+              listItem: 'Грузоподъемность 1,5 т',
+            },
+          ],
+          btn: '',
+          autoImg: 'auto-02.png',
+        },
+      ],
+      autoparkCurrentSlide: 0,
+      rewiesItems: [
+        {
+          title: 'Ирина',
+          id: 'rewiesItem-1',
+          data: '24.03.2019',
+          comment: 'Обращение к вам за помощью в переезде оставило только положительные впечатления. Хотелось отметить замечательного водителя газели Алексея. Были трудности с заездом во двор, он проявил себя как компетентный и профессиональный сотрудник, за что ему огромное спасибо! Могу на 100% порекомендовать компанию MOVEE и отметить, что цены фиксированы, что упрощает расчет.',
+        },
+        {
+          title: 'Светлана Васильевна ',
+          id: 'rewiesItem-2',
+          comment: 'Сегодня ( 13.09.2019 ) мы переехали. Остались очень довольны, несмотря на то, что к нам опоздали на 1,5 часа. Морально были к этому готовы ( в прошлый переезд в другой фирме тоже опоздали на час). Водитель заранее позвонил, предупредил, что задерживается. Когда заранее позвонил и предупредил! Могу на 100% порекомендовать компанию MOVEE и отметить, что цены фиксированы, что упрощает расчет.',
+          data: '13.09.2019',
+        },
+        {
+          title: 'Ольга Петрова',
+          id: 'rewiesItem-3',
+          data: '11.11.2019',
+          comment: 'Могу на 100% порекомендовать компанию MOVEE и отметить, что цены фиксированы, что упрощает расчет. Безо всяких заморочек, в отличии от их конкурентов. Упаковали все качественно и быстро. Все вещи приехали в сохранности. Особлая благодарность Александру за его уситчивость! Могу на 100% порекомендовать компанию MOVEE и отметить, что цены фиксированы, что упрощает расчет.',
+        },
+      ],
       pricesTitle: 'Цены',
       pricesSubitle: 'Фиксированная стоимость на переезд под ключ',
       pricesCards: [
@@ -89,12 +184,87 @@ const App = {
             metaTime: 'Переезд за 6 часов',
             price: '16 490₽',
           },
-      ]
+      ],
+      contactTitle: 'Закажи  переезд прямо сейчас',
+      contactSubtitle: 'Мы перезвоним в течении 10 секунд',
+      mapLinks: [
+        {
+          title: 'Москва, Волгоградский пр-т, 38'
+        },
+        {
+          title: '8 (800)-080-12-13'
+        },
+        {
+          title: 'hello@movee.ru'
+        },
+      ],
+      footerLinkPolit: 'Политика конфиденциальнности',
+      footerLinkCopy: 'Соглашение на обработку персональных данных',
+      footerLinks: [
+        {
+          title: 'Главная',
+          link: '#'
+        },
+        {
+          title: 'Цена',
+          link: '#'
+        },
+        {
+          title: 'Автопарк',
+          link: '#'
+        },
+        {
+          title: 'Отзывы',
+          link: '#'
+        },
+        {
+          title: 'Контакты',
+          link: '#'
+        },
+      ],
     };
   },
   methods: {
-    fire() {
-      console.log("fire!");
+    textSize (num, el, choise, index) {
+      let cropElement = el, // выбор элементов 
+            size = num,                                             // кол-во символов 
+            endCharacter = '...';
+
+        if (cropElement.length > 1) {
+          cropElement.forEach(el => {
+            let text = el.innerHTML;
+            
+            if (el.innerHTML.length > size) {
+                text = text.substr(0, size);
+                el.innerHTML = text + endCharacter;
+            }
+          });
+        } else {
+          let text = vm.rewiesItems[index].comment;
+          text = text.substr(0, size);
+          cropElement.innerHTML = text;
+
+          if (choise == true) {
+            cropElement.innerHTML = text;
+          } else {
+            cropElement.innerHTML = text + endCharacter;
+          }
+        }
+    },
+    reviewsbtn (e) {
+      if (e.target.innerHTML === 'Скрыть') {
+        e.target.innerHTML = 'Читать полностью';
+        let elem = e.target.closest('.reviews__table-item').querySelector('.reviews__table-item-text');
+        let elemItemId = e.target.closest('.reviews__table-item').id;
+        let index = vm.rewiesItems.findIndex(el => el.id === elemItemId);
+        vm.textSize(260, elem, false, index)
+      } else {
+        e.target.innerHTML = 'Скрыть';
+        let elem = e.target.closest('.reviews__table-item').querySelector('.reviews__table-item-text');
+        let elemItemId = e.target.closest('.reviews__table-item').id;
+        let index = vm.rewiesItems.findIndex(el => el.id === elemItemId);
+        vm.textSize(900, elem, true, index)
+      }
     },
     pricesBtn(e) {
       e.target.classList.add("active");
@@ -102,7 +272,85 @@ const App = {
         e.target.classList.remove("active");
       }, 1000);
     },
-  },
-};
+    reviewMain () {
+      let cropEl = document.querySelectorAll('.reviews__table-item-text');
+      vm.textSize(260, cropEl);
+    },
+    anchors () {
+      const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+      animationTime = 1000,
+      framesCount = 400;
 
-Vue.createApp(App).mount("#page");
+        anchors.forEach(function(item) {
+          // каждому якорю присваиваем обработчик события
+          item.addEventListener('click', function(e) {
+            // убираем стандартное поведение
+            e.preventDefault();
+            
+            // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+            
+            // запускаем интервал, в котором
+            let scroller = setInterval(function() {
+              // считаем на сколько скроллить за 1 такт
+              let scrollBy = coordY / framesCount;
+              
+              // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+              // и дно страницы не достигнуто
+              if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                // то скроллим на к-во пикселей, которое соответствует одному такту
+                window.scrollBy(0, scrollBy);
+              } else {
+                // иначе добираемся до элемента и выходим из интервала
+                window.scrollTo(0, coordY);
+                clearInterval(scroller);
+              }
+            // время интервала равняется частному от времени анимации и к-ва кадров
+            }, animationTime / framesCount);
+  });
+});
+    },
+    slidePrev() {
+      if (this.autoparkCurrentSlide > 0) {
+        this.autoparkCurrentSlide--
+      }
+    },
+    slideNext() {
+      if (this.autoparkCurrentSlide >= this.autoparkCarouselSlide.length - 1) {
+        this.autoparkCurrentSlide = 0;
+      } else {
+        this.autoparkCurrentSlide++
+      }
+    },
+    slideNav () {
+      const carouselNavItem = document.querySelectorAll('.autopark__carousel-nav-item');
+
+      for (let index = 0; index < carouselNavItem.length; index++) {
+        const element = carouselNavItem[index];
+        if (index === this.autoparkCurrentSlide) {
+          element.classList.add('active');
+        } else {
+          element.classList.remove('active');
+        }
+      }
+    }
+  },
+  mounted() {
+    window.onload = () => {
+      const carouselNavItem = document.querySelectorAll('.autopark__carousel-nav-item');
+
+      for (let index = 0; index < carouselNavItem.length; index++) {
+        const element = carouselNavItem[index];
+        if (index === this.autoparkCurrentSlide) {
+          element.classList.add('active');
+        } else {
+          element.classList.remove('active');
+        }
+      }
+    }
+  },
+});
+
+const vm = app.mount("#page");
+vm.reviewMain();
+vm.anchors();
